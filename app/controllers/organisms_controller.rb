97500f7048c21f193b3bea4c8459664ef6347936
nakanishi <ddaiyyummig@gmail.comm>
2023-01-21 16:32:11 +0900

    add pagenation, download ctr

--- app/controllers/organisms_controller.rb
+++ app/controllers/organisms_controller.rb
@@ -1,18 +1,24 @@
 class OrganismsController < ApplicationController
+  @@tmp = String.new
   
   def top
   end
 
   def database_show
-    @organisms = Organism.all
+    @organisms = Organism.all.page(params[:page])
   end
 
   def create
     @acc = params[:acc]
+    @@tmp = @acc
+  end
+
+  def download
+    send_file "app/assets/images/#{@@tmp}.png" 
   end
 
   def import
     Organism.import(params[:file])
-    redirect_to organisms_url
+    redirect_to database_show_path
   end
 end
