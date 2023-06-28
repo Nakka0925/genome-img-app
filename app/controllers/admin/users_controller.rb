714d9c884bc686b0fd72e52f769ba6e32063c41a
Nakka0925 <h23ms413@hirosaki-u.ac.jp>
2023-06-28 17:56:52 +0900

    Add logout and admin function

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,7 @@
 class Admin::UsersController < ApplicationController
+
+  before_action :require_admin
+
   def index
     @users = User.all
   end
@@ -29,7 +32,7 @@ class Admin::UsersController < ApplicationController
     @user = User.find(params[:id])
 
     if @user.update(user_params)
-      redirect_to admin_user_url(@user), notice: "ユーザー「#{user.name}」を更新しました。"
+      redirect_to admin_user_url(@user), notice: "ユーザー「#{@user.name}」を更新しました。"
     else
       render :edit
     end
@@ -43,6 +46,10 @@ class Admin::UsersController < ApplicationController
 
   private
 
+  def require_admin
+    redirect_to root_path unless current_user.admin?
+  end
+  
   def user_params
     params.require(:user).permit(:name, :email, :admin, :password, :password_confirmation)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :users
   end
+
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+
   root to: 'organisms#top'
   get 'download', to: 'organisms#download', as: :download
   post 'create', to: 'organisms#create', as: :create
